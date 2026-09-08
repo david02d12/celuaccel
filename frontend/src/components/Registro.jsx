d028d66d1c68c833dd44ed1d683fe7a7a90cd12d
Anderson David Joya Peña <david02d12@gmail.com>
2026-09-08 01:23:29 -0500

    fix: importar useState en Registro.jsx

--- frontend/src/components/Registro.jsx
+++ frontend/src/components/Registro.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRegistroForm } from '../hooks/useRegistroForm';
 
 const Registro = ({ setModoRegistro, setVista }) => {
